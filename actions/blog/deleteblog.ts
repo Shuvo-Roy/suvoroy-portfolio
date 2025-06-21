70c6f3d585110bd259bffb4bca59cb45b2de219d
Shuvo-Roy <shuvoroysp@gmail.com>
2025-06-21 23:28:45 +0600

    error solve

--- actions/blog/deleteblog.ts
+++ actions/blog/deleteblog.ts
@@ -1,11 +1,12 @@
-"use server"
+"use server";
 
-import { prisma } from "@/lib/prisma"
-import { revalidatePath } from "next/cache"
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
-export const deleteBlog = async (blogId:string)=>{
-    await prisma.education.delete({
-        where:{ id:blogId}
-    })
-    revalidatePath("/dashboard")
-}
+export const deleteBlog = async (blogId: string) => {
+  await prisma.blog.delete({
+    where: { id: blogId },
+  });
+
+  revalidatePath("/dashboard");
+};
