77b1a4271d33173723e71b9cbd65c53335e50f59
Shuvo-Roy <shuvoroysp@gmail.com>
2025-06-21 20:01:40 +0600

    ..

--- src/app/dashboard/educations/[id]/edit/page.tsx
+++ src/app/dashboard/educations/[id]/edit/page.tsx
@@ -1,20 +1,19 @@
-
 import EditEducationPage from '@/components/dashboard-components/educations/EditEducationPage';
 import { prisma } from '@/lib/prisma';
 import React from 'react';
 
-type EditEducationParams = {
-  params: { id: string };
-};
+type Params = Promise<{ id: string }>;
 
-const Page: React.FC<EditEducationParams> = async ({ params }) => {
+const Page = async ({ params }: { params: Params }) => {
   const { id } = await params;
 
   const education = await prisma.education.findUnique({
     where: { id },
   });
 
-  if (!education) return <h1>Education record not found for this ID</h1>;
+  if (!education) {
+    return <h1>Education record not found for this ID</h1>;
+  }
 
   return (
     <div>
